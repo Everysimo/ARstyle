afed9f944e7dfe181497bd911d9f3237590ce759
dany98sa <demarcodaniele98@gmail.com>
2022-07-20 19:15:00 +0200

    feat: add assets logo

---
+++ ARStyle/view/SplashScreen.swift
@@ -0,0 +1,22 @@
+//
+//  SplashScreen.swift
+//  ARStyle
+//
+//  Created by Daniele De Marco on 20/07/22.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
+
+struct SplashScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreen()
+    }
+}
